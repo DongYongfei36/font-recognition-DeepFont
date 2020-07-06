642e0547877ae4c79932afa6929041f597d3db40
tangji08 <ji.tang@polytechnique.edu>
2020-07-06 22:11:33 +0200

    add tools

---
+++ tools.py
@@ -0,0 +1,134 @@
+import os
+import io
+import typing
+import hashlib
+import urllib.request
+import urllib.parse
+
+import cv2
+import imgaug
+import numpy as np
+import validators
+import matplotlib.pyplot as plt
+from shapely import geometry
+from scipy import spatial
+
+def read(filepath_or_buffer: typing.Union[str, io.BytesIO]):
+    """Read a file into an image object
+
+    Args:
+        filepath_or_buffer: The path to the file, a URL, or any object
+            with a `read` method (such as `io.BytesIO`)
+    """
+    if isinstance(filepath_or_buffer, np.ndarray):
+        return filepath_or_buffer
+    if hasattr(filepath_or_buffer, 'read'):
+        image = np.asarray(bytearray(filepath_or_buffer.read()), dtype=np.uint8)
+        image = cv2.imdecode(image, cv2.IMREAD_UNCHANGED)
+    elif isinstance(filepath_or_buffer, str):
+        if validators.url(filepath_or_buffer):
+            return read(urllib.request.urlopen(filepath_or_buffer))
+        assert os.path.isfile(filepath_or_buffer), \
+            'Could not find image at path: ' + filepath_or_buffer
+        image = cv2.imread(filepath_or_buffer)
+    return cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+def drawImage(image, ax=None):
+    """Draw image.
+
+    Args:
+        image: The image to draw
+        ax: A matplotlib axis on which to draw.
+    """
+    if ax is None:
+        _, ax = plt.subplots(figsize=(6,6), dpi=100)
+    ax.imshow(image)
+    ax.set_yticks([])
+    ax.set_xticks([])
+
+def warpBox(image,
+            box,
+            target_height=None,
+            target_width=None,
+            margin=0,
+            cval=None,
+            return_transform=False,
+            skip_rotate=False):
+    """Warp a boxed region in an image given by a set of four points into
+    a rectangle with a specified width and height. Useful for taking crops
+    of distorted or rotated text.
+
+    Args:
+        image: The image from which to take the box
+        box: A list of four points starting in the top left
+            corner and moving clockwise.
+        target_height: The height of the output rectangle
+        target_width: The width of the output rectangle
+        return_transform: Whether to return the transformation
+            matrix with the image.
+    """
+    if cval is None:
+        cval = (0, 0, 0) if len(image.shape) == 3 else 0
+    if not skip_rotate:
+        box, _ = get_rotated_box(box)
+    w, h = get_rotated_width_height(box)
+    assert (
+        (target_width is None and target_height is None)
+        or (target_width is not None and target_height is not None)), \
+            'Either both or neither of target width and height must be provided.'
+    if target_width is None and target_height is None:
+        target_width = w
+        target_height = h
+    scale = min(target_width / w, target_height / h)
+    M = cv2.getPerspectiveTransform(src=box,
+                                    dst=np.array([[margin, margin], [scale * w - margin, margin],
+                                                  [scale * w - margin, scale * h - margin],
+                                                  [margin, scale * h - margin]]).astype('float32'))
+    crop = cv2.warpPerspective(image, M, dsize=(int(scale * w), int(scale * h)))
+    target_shape = (target_height, target_width, 3) if len(image.shape) == 3 else (target_height,
+                                                                                   target_width)
+    full = (np.zeros(target_shape) + cval).astype('uint8')
+    full[:crop.shape[0], :crop.shape[1]] = crop
+    if return_transform:
+        return full, M
+    return full
+
+def drawBoxes(image, boxes, color=(255, 0, 0), thickness=5, boxes_format='boxes'):
+    """Draw boxes onto an image.
+
+    Args:
+        image: The image on which to draw the boxes.
+        boxes: The boxes to draw.
+        color: The color for each box.
+        thickness: The thickness for each box.
+        boxes_format: The format used for providing the boxes. Options are
+            "boxes" which indicates an array with shape(N, 4, 2) where N is the
+            number of boxes and each box is a list of four points) as provided
+            by `keras_ocr.detection.Detector.detect`, "lines" (a list of
+            lines where each line itself is a list of (box, character) tuples) as
+            provided by `keras_ocr.data_generation.get_image_generator`,
+            or "predictions" where boxes is by itself a list of (word, box) tuples
+            as provided by `keras_ocr.pipeline.Pipeline.recognize` or
+            `keras_ocr.recognition.Recognizer.recognize_from_boxes`.
+    """
+    if len(boxes) == 0:
+        return image
+    canvas = image.copy()
+    if boxes_format == 'lines':
+        revised_boxes = []
+        for line in boxes:
+            for box, _ in line:
+                revised_boxes.append(box)
+        boxes = revised_boxes
+    if boxes_format == 'predictions':
+        revised_boxes = []
+        for _, box in boxes:
+            revised_boxes.append(box)
+        boxes = revised_boxes
+    for box in boxes:
+        cv2.polylines(img=canvas,
+                      pts=box[np.newaxis].astype('int32'),
+                      color=color,
+                      thickness=thickness,
+                      isClosed=True)
+    return canvas
